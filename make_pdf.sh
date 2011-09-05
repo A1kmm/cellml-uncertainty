242aaa15ddc7257c4d104ecfd613c563638c24d8
Andrew Miller <ak.miller@auckland.ac.nz>
2011-09-05 13:35:17 +1200

    Set up a system for generating PDFs with word wrapping enabled, matching what was done with the proposal for uncertainty in FieldML.

--- make_pdf.sh
+++ make_pdf.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-fop -catalog -param section.autolabel 1 -param section.label.includes.component.label 1 -param page.orientation portrait -param paper.type A4 -xml ./CellML-Uncertainty.docbook -xsl /home/amil082/docbook-xsl-1.76.1/fo/docbook.xsl CellML-Uncertainty.pdf
+fop -catalog -xml ./CellML-Uncertainty.docbook -xsl to-fo.xsl CellML-Uncertainty.pdf
